9a8d9b3fcfa9920e9a691f8d729b0a0d934c6d27
Andrew-M410 <andrewmaksimov410@yandex.ru>
2026-03-26 19:05:03 +0300

    ready v9

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@
  */
 function calculateSimpleRevenue(purchase, _product) { // Расчет выручки от операции
     const { discount, sale_price, quantity } = purchase;
-    return sale_price * quantity * (1 - discount / 100);
+    return sale_price * quantity * (1 - (discount / 100));
 }
 
 
